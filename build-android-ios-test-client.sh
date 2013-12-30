36cb704aa10aee65f0b36f45f4892c4712569a96
cesine <cesine@yahoo.com>
2013-12-29 22:03:48 -0500

    recursively deleting non load.js files from builds packages to reduce apk size

--- build-android-ios-test-client.sh
+++ build-android-ios-test-client.sh
@@ -7,6 +7,11 @@ rm -rf mobileclient
 rm -rf builds
 mop
 
+echo "Removing extra stuff from builds to reduce apk size"
+cd  builds/radio-player/packages
+find . -name  "*[^d].js" -delete
+find . -name  "*.md" -delete
+
 cd $CURRENT
 echo "Creating a cordova app, for more info see http://cordova.apache.org/docs/en/3.3.0/guide_cli_index.md.html#The%20Command-Line%20Interface"
 
