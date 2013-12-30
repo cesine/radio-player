e2ffdca8b39e0b0a23549599a96781d5cf6e120d
cesine <cesine@yahoo.com>
2013-12-29 21:08:21 -0500

    remove the android client, otherwise it gets mopped too

--- build-android-ios-test-client.sh
+++ build-android-ios-test-client.sh
@@ -3,11 +3,12 @@
 
 CURRENT=`pwd`
 
-#mop
+rm -rf mobileclient
+rm -rf builds
+mop
 
 cd $CURRENT
 echo "Creating a cordova app, for more info see http://cordova.apache.org/docs/en/3.3.0/guide_cli_index.md.html#The%20Command-Line%20Interface"
-rm -rf mobileclient
 
 cordova create mobileclient com.example.test.client TestClient
 cd mobileclient
