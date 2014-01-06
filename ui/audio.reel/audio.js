328dad3b1d7f9bfabd0d010bfb57a69bb3b783de
cesine <cesine@yahoo.com>
2014-01-06 18:10:29 -0500

    roughdraft of decoding

--- ui/audio.reel/audio.js
+++ ui/audio.reel/audio.js
@@ -81,5 +81,32 @@ exports.Audio = Component.specialize( /** @lends Audio# */ {
 				// this._audioElement.currentTime = 0;
 			}
 		}
+	},
+
+	decode: {
+		value: function(formatIn, formatOut) {
+			// var myAudioContext = new webkitAudioContext();
+
+			// function bufferSound(event) {
+			// 	var request = event.target;
+			// 	var source = myAudioContext.createBufferSource();
+			// 	source.buffer = myAudioContext.createBuffer(request.response, false);
+			// 	source.connect(myAudioContext.destination);
+			// 	source.noteOn(0);
+			// }
+
+			// var request = new XMLHttpRequest();
+			// request.open('GET', 'http://178.32.57.58:8817/;', true);
+			// request.responseType = 'arraybuffer';
+			// request.addEventListener('load', bufferSound, false);
+			// request.addEventListener('error', function(error) {
+			// 	console.log("error loading stream " + error);
+			// }, false);
+			// request.onload = bufferSound;
+			// request.onerror = function(e, f, g) {
+			// 	console.log(e, f, g);
+			// };
+			// request.send();
+		}
 	}
 });
